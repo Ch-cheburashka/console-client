9d27887ac7497402e2c25a65d43ee630b1b56896
Ch-cheburashka <m9056883999@gmail.com>
2023-03-28 20:05:15 +0300

    added file search to the functions

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
 #include <filesystem>
+#include <string>
 #include <boost/program_options.hpp>
 
 using fpath = std::filesystem::path;
 
-void download(const fpath& path) {
-    if (exists(path))
+void download(const std::string& file) {
+    const fpath p {std::filesystem::current_path()};
+    bool found = false;
+    for (const std::filesystem::directory_entry& x : std::filesystem::directory_iterator{p}) {
+        auto entry_str = std::string(x.path());
+        if (entry_str.substr(entry_str.find_last_of('/') + 1) == file)
+            found = true;
+    }
+    if (found)
         std::cout << "such file already exists\n";
     else
-        std::cout << "downloading of file: \"" << path << "\" started\n";
+        std::cout << "downloading of file: \"" << file << "\" started\n";
 }
 
-void upload(const fpath& path) {
-    if (!exists(path))
+void upload(const std::string& file) {
+    const fpath p {std::filesystem::current_path()};
+    bool found = false;
+    for (const std::filesystem::directory_entry& x : std::filesystem::directory_iterator{p}) {
+        auto entry_str = std::string(x.path());
+        if (entry_str.substr(entry_str.find_last_of('/') + 1) == file)
+            found = true;
+    }
+    if (!found)
         std::cout << "such file doesn't exists\n";
     else
-        std::cout << "uploading of file: \"" << path << "\" started\n";
+        std::cout << "uploading of file: \"" << file << "\" started\n";
 }
 
 
@@ -27,7 +42,7 @@ int main(int argc, char **argv) {
             ("help", "produce help message")
             ("download","download the given file")
             ("upload", "upload the given file")
-            ("filename",boost::program_options::value<fpath>()->required(),"set filename");
+            ("filename",boost::program_options::value<std::string>()->required(),"set filename");
 
     boost::program_options::variables_map vm;
     boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
@@ -37,16 +52,15 @@ int main(int argc, char **argv) {
         std::cout << desc << "\n";
         return 1;
     }
-    std::filesystem::path p;
-
+    std::string file;
     if (vm.count("filename"))
-        p = vm["filename"].as<fpath>();
+        file = vm["filename"].as<std::string>();
 
     if (vm.count("download")) {
-        download(p);
+        download(file);
     }
 
     if (vm.count("upload"))
-        upload(p);
+        upload(file);
     return 0;
 }
